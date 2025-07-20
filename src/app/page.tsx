f48ba22102af02967a0ac6020be176ffa9e11f55
theboomiest <pmrodden4@gmail.com>
2025-07-20 17:54:57 -0600

    Fix page width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { SignupForm } from "./ui/signup-form";
-import { AddFriendForm, db, FriendList } from "./actions/db";
+import { AddUserForm, db, UserList } from "./actions/db";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1 className="text-3xl">Omnira - Omniscient + era/aura</h1>
-        <h2>Add Friend</h2>
-        <AddFriendForm defaultAge={21} />
+        <h1 className="text-3xl">Omnira</h1>
+        <h2>Add User</h2>
+        <AddUserForm />
 
-        <h2>Friend List</h2>
-        <FriendList minAge={18} maxAge={65} />
+        <h2 className="w-[50vw]">User List</h2>
+        <UserList />
       </main>
       <footer className={styles.footer}>
         
