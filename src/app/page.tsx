f61d20f8c32ef0e56aa9120227a45564d9e8f0e6
theboomiest <pmrodden4@gmail.com>
2025-07-20 19:25:15 -0600

    Add test phosphor icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { SignupForm } from "./ui/signup-form";
-import { AddUserForm, db, UserList } from "./actions/db";
+import { AddUserForm, AddDomainForm, db, UserList } from "./actions/db";
+import { PlanetIcon } from "@phosphor-icons/react/ssr";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1 className="text-3xl">Omnira</h1>
+        <span>
+          <PlanetIcon size="24px"/><p>Add Domain</p>
+        </span>
+        <AddDomainForm />
         <h2>Add User</h2>
         <AddUserForm />
-
         <h2 className="w-[50vw]">User List</h2>
         <UserList />
       </main>
